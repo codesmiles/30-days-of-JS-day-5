94dfc97ae4f10a0356a9602bf1a6518fae24ca41
Michael Nwogu <mikedbchi@gmail.com>
2021-08-28 16:41:18 -0700

    Ultimate update

--- script.js
+++ script.js
@@ -132,20 +132,68 @@ const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
 const backEnd = ['Node','Express', 'MongoDB']
 const fullStack = frontEnd.concat(backEnd);
 console.log(fullStack)
+
 /*
 The following is an array of 10 students ages:
 
 const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
-Sort the array and find the min and max age
-Find the median age(one middle item or two middle items divided by two)
-Find the average age(all items divided by number of items)
-Find the range of the ages(max minus min)
-Compare the value of (min - average) and (max - average), use abs() method 1.Slice the first ten countries from the countries array*/
-
+Sort the array and find the min and max age*/
 const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
+
 console.log(ages.sort())
+
 let agesMin = ages[0];
 console.log(agesMin);
 
 let agesMax = ages[ages.length-1]
 console.log(agesMax)
+
+
+//Find the median age(one middle item or two middle items divided by two)
+if(ages.length % 2 !== 0){
+	let result = ages[Math.round(ages.length/2)]
+	console.log(result)
+	}
+	else{
+	let result = (ages[(ages.length/2)] + ages[(ages.length/2) + 1])/2
+	console.log(result)
+	}
+
+// Find the average age(all items divided by number of items)
+
+// I did the native method lol
+let average = (ages[0] + ages[1] + ages[2] + ages[3] + ages[4] + ages[5] + ages[6] + ages[7] + ages[8] + ages[9])/10
+
+console.log(average);
+
+
+// Find the range of the ages(max minus min)
+let range = agesMax - agesMin;
+console.log(range);
+
+
+// Compare the value of (min - average) and (max - average), use abs() method 
+
+if(Math.abs(agesMin - average === agesMax-average)){
+	console.log(`They are equal`)
+	}
+	else{
+	console.log(`not equal`)
+	}
+
+// 1.Slice the first ten countries from the countries array
+console.log(countries.slice(9,10))
+
+
+// Find the middle country(ies) in the countries array
+console.log(countries[(countries.length-1)/2])
+
+// Divide the countries array into two equal arrays if it is even. If countries array is not even , one more country for the first half.
+if(countries.length / 2 !== 0){
+	let firstSlice = countries.slice(0 , Math.round(countries.length/2))
+	let secondSlice = countries.slice(Math.round(countries.length/2) , countries.length)
+	console.log(firstSlice, secondSlice);
+	}else{
+	let firstSlice = countries.slice(0 , countries.length/2)
+	let secondSlice = countries.slice( (countries.length/2) , countries.length)
+	}
