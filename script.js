b11bc586857bb79408f1c14e801123eafc8d6e39
Michael Nwogu <mikedbchi@gmail.com>
2021-08-28 15:02:57 -0700

    Update

--- script.js
+++ script.js
@@ -132,3 +132,20 @@ const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
 const backEnd = ['Node','Express', 'MongoDB']
 const fullStack = frontEnd.concat(backEnd);
 console.log(fullStack)
+/*
+The following is an array of 10 students ages:
+
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+Sort the array and find the min and max age
+Find the median age(one middle item or two middle items divided by two)
+Find the average age(all items divided by number of items)
+Find the range of the ages(max minus min)
+Compare the value of (min - average) and (max - average), use abs() method 1.Slice the first ten countries from the countries array*/
+
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
+console.log(ages.sort())
+let agesMin = ages[0];
+console.log(agesMin);
+
+let agesMax = ages[ages.length-1]
+console.log(agesMax)
