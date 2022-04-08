adcf8b9945c52ffbc793baf6d24eb9f7550e3e25
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-04-08 23:19:04 +0100

    update

--- main.js
+++
@@ -1,4 +0,0 @@
-import webTechs from ./web_techs.js;
-import countries from ./countries.js;
-
-console.log(countries);
---
+++ main.mjs
@@ -0,0 +1,4 @@
+import  webTechs  from "./web_techs.mjs";
+import  countries  from "./countries.mjs";
+console.log(countries)
+console.log(webTechs)
